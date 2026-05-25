adcd9ebd43c62185dc2d20c4ca9e6dd114e44415
san4EZzzZZZ <zuev.aleksandr.dstu@gmail.com>
2026-05-25 11:28:09 +0300

    feat: update API URL and add expo-build-properties dependency

--- centr-invest-app/src/api/client.js
+++ centr-invest-app/src/api/client.js
@@ -19,7 +19,7 @@ function getDefaultApiUrl() {
   return Platform.OS === 'android' ? 'http://10.0.2.2:8080/api' : 'http://localhost:8080/api';
 }
 
-export const API_URL = process.env.EXPO_PUBLIC_API_URL || getDefaultApiUrl();
+export const API_URL = process.env.EXPO_PUBLIC_API_URL || 'http://212.119.42.207:8080/api';
 
 let memoryToken = null;
 
